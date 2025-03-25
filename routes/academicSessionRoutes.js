2b99facbae5b47ae14abc841958c72b4db7fa377
azeezfasasi <fas.azeez@gmail.com>
2025-03-25 08:18:00 +0100

    es

--- routes/academicSessionRoutes.js
+++ routes/academicSessionRoutes.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const { createSession, getAllSessions, getCurrentSession, createTerm, getTermsBySession, getCurrentTerm } = require("../controllers/academicController");
 
 // Create a new academic session
-router.post("/create", createSession);
-router.get("/all", getAllSessions);
-router.get("/current", getCurrentSession);
-router.post("/create", createTerm);
-router.get("/:sessionId", getTermsBySession);
-router.get("/current", getCurrentTerm);
+router.post("/session/create", createSession); 
+router.get("/session/all", getAllSessions);
+router.get("/session/current", getCurrentSession);
+router.post("/term/create", createTerm);
+router.get("/term/:sessionId", getTermsBySession); 
+router.get("/term/current", getCurrentTerm);
 
 module.exports = router;
